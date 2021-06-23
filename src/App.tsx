7749972b471c115790537e86edc1e15c42839df0
Maksym Honchar <maksim.honchar@gmail.com>
2021-06-23 13:01:52 +0300

    added webpack

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function App() {
+    return(
+        <h2>React Webpack</h2>
+    )
+}
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+mode: 'development',
+entry: {
+   index: './src/index.tsx',
+},
+module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+devtool: 'inline-source-map',
+devServer: {
+    contentBase: './dist',
+    hot: true,
+  },
+plugins: [
+    new HtmlWebpackPlugin({
+     template: './public/index.html'
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+};
