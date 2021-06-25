79fff8c641f3952e595180028b2c0c0e26e6cf05
Maksym Honchar <maksim.honchar@gmail.com>
2021-06-25 10:49:01 +0300

    added movie option

--- src/controllers/AddMovieController.tsx
+++ src/controllers/AddMovieController.tsx
@@ -15,7 +15,38 @@ export const AddMovieController: FC = () => {
   const [format, setFormat] = useState('');
   const [firstActor, setFirstActor] = useState({ firstName: '', lastName: '' });
   const [secondActor, setSecondActor] = useState({ firstName: '', lastName: '' });
+  const [thirdActor, setThirdActor] = useState({ firstName: '', lastName: '' });
+  const [fourthActor, setFourthActor] = useState({ firstName: '', lastName: '' });
+  const [fifthActor, setFifthActor] = useState({ firstName: '', lastName: '' });
+  const [actorCounter, setActorCounter] = useState(0);
+
   const year = +yearRelease;
+
+  const yearLength = yearRelease.toString().length > 3;
+
+  const canSubmit = [
+    movieTitle, yearLength, format, firstActor.firstName, firstActor.lastName,
+  ].every(Boolean);
+
+  const moreThanZero = canSubmit && actorCounter > 0;
+  const counterEqualOne = moreThanZero && actorCounter === 1;
+  const secondActorFilled = [
+    moreThanZero, secondActor.firstName, secondActor.lastName,
+  ].every(Boolean) && actorCounter === 1;
+
+  const moreThanOne = moreThanZero && actorCounter > 1;
+  const counterEqualTwo = moreThanOne && actorCounter === 2;
+  const thirdActorFilled = [
+    moreThanOne, thirdActor.firstName, thirdActor.lastName,
+  ].every(Boolean) && actorCounter === 2;
+
+  const moreThanTwo = moreThanOne && actorCounter > 2;
+  const counterEqualThree = moreThanTwo && actorCounter === 3;
+  const fourthActorFilled = [
+    moreThanTwo, fourthActor.firstName, fourthActor.lastName,
+  ].every(Boolean) && actorCounter === 3;
+
+  const moreThanThree = moreThanTwo && actorCounter > 3;
 
   const handleSetMovie = (e: ChangeEvent<HTMLInputElement>) => setMovieTitle(e.target.value);
   const handleSetRelease = (e: ChangeEvent<{ value: number}>) => setYearRelease(e.target.value);
@@ -29,15 +60,31 @@ export const AddMovieController: FC = () => {
     { target: { name, value } }:ChangeEvent<HTMLInputElement>,
   ) => setSecondActor({ ...secondActor, [name]: value });
 
+  const handleThirdActor = (
+    { target: { name, value } }:ChangeEvent<HTMLInputElement>,
+  ) => setThirdActor({ ...thirdActor, [name]: value });
+
+  const handleFourthActor = (
+    { target: { name, value } }:ChangeEvent<HTMLInputElement>,
+  ) => setFourthActor({ ...fourthActor, [name]: value });
+
+  const handleFifthActor = (
+    { target: { name, value } }:ChangeEvent<HTMLInputElement>,
+  ) => setFifthActor({ ...fifthActor, [name]: value });
+
   const handleSubmit = (e: SyntheticEvent) => {
     e.preventDefault();
-    const cast = [firstActor, secondActor];
+    const cast = [
+      firstActor, secondActor, thirdActor, fourthActor, fifthActor,
+    ].filter((actor) => actor.firstName && actor.lastName);
+
     const film = {
       movieTitle, year, format, cast, id: nanoid(),
     };
-
     dispatch(addMovie(film));
   };
+
+  const addCount = () => setActorCounter(actorCounter + 1);
 
   return (
     <>
@@ -52,7 +99,25 @@ export const AddMovieController: FC = () => {
         handleFirstActor={handleFirstActor}
         secondActor={secondActor}
         handleSecondActor={handleSecondActor}
+        thirdActor={thirdActor}
+        handleThirdActor={handleThirdActor}
         handleSubmit={handleSubmit}
+        canSubmit={canSubmit}
+        addCount={addCount}
+        moreThanZero={moreThanZero}
+        counterEqualOne={counterEqualOne}
+        secondActorFilled={secondActorFilled}
+        moreThanOne={moreThanOne}
+        counterEqualTwo={counterEqualTwo}
+        thirdActorFilled={thirdActorFilled}
+        fourthActor={fourthActor}
+        handleFourthActor={handleFourthActor}
+        moreThanTwo={moreThanTwo}
+        counterEqualThree={counterEqualThree}
+        fourthActorFilled={fourthActorFilled}
+        fifthActor={fifthActor}
+        moreThanThree={moreThanThree}
+        handleFifthActor={handleFifthActor}
       />
     </>
   );
--- src/view/AddMovieForm.tsx
+++ src/view/AddMovieForm.tsx
@@ -7,6 +7,9 @@ import InputLabel from '@material-ui/core/InputLabel';
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
 import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
+import Tooltip from '@material-ui/core/Tooltip';
 
 const useStyles = makeStyles({
   root: {
@@ -28,6 +31,9 @@ const useStyles = makeStyles({
   inputFieldActor: {
     width: 140,
   },
+  plusButton: {
+    margin: '-10px 0 5px 0',
+  },
   btnSubmit: {
     width: 300,
   },
@@ -44,7 +50,25 @@ interface IAddMovieForm {
     handleFirstActor: ({ target: { name, value } }:ChangeEvent<HTMLInputElement>) => void
     secondActor: {firstName: string, lastName: string}
     handleSecondActor: ({ target: { name, value } }:ChangeEvent<HTMLInputElement>) => void
+    thirdActor: {firstName: string, lastName: string}
+    handleThirdActor: ({ target: { name, value } }:ChangeEvent<HTMLInputElement>) => void
+    fourthActor: {firstName: string, lastName: string}
+    handleFourthActor: ({ target: { name, value } }:ChangeEvent<HTMLInputElement>) => void
+    fifthActor: {firstName: string, lastName: string}
+    handleFifthActor: ({ target: { name, value } }:ChangeEvent<HTMLInputElement>) => void
     handleSubmit: (e: SyntheticEvent) => void
+    canSubmit: boolean
+    addCount: () => void
+    moreThanZero: boolean
+    counterEqualOne: boolean
+    secondActorFilled: boolean
+    moreThanOne: boolean
+    counterEqualTwo: boolean
+    thirdActorFilled: boolean
+    moreThanTwo: boolean
+    counterEqualThree: boolean
+    fourthActorFilled: boolean
+    moreThanThree: boolean
 }
 
 export const AddMovieForm: FC<IAddMovieForm> = (props) => {
@@ -60,7 +84,25 @@ export const AddMovieForm: FC<IAddMovieForm> = (props) => {
     handleFirstActor,
     secondActor,
     handleSecondActor,
+    thirdActor,
+    handleThirdActor,
+    fourthActor,
+    handleFourthActor,
+    fifthActor,
+    handleFifthActor,
     handleSubmit,
+    canSubmit,
+    addCount,
+    moreThanZero,
+    secondActorFilled,
+    counterEqualOne,
+    moreThanOne,
+    counterEqualTwo,
+    thirdActorFilled,
+    moreThanTwo,
+    counterEqualThree,
+    fourthActorFilled,
+    moreThanThree,
   } = props;
 
   return (
@@ -74,6 +116,7 @@ export const AddMovieForm: FC<IAddMovieForm> = (props) => {
             className={classes.inputField}
             value={movieTitle}
             onChange={handleSetMovie}
+            required
           />
         </div>
         <div className={classes.inputDiv}>
@@ -84,15 +127,17 @@ export const AddMovieForm: FC<IAddMovieForm> = (props) => {
             value={yearRelease}
             onChange={handleSetRelease}
             type="number"
+            required
           />
         </div>
         <FormControl variant="outlined">
-          <InputLabel>format</InputLabel>
+          <InputLabel required>format</InputLabel>
           <Select
             className={classes.inputField}
             value={format}
             onChange={handleSetFormat}
             label="format"
+            required
           >
             <MenuItem value="VHS">VHS</MenuItem>
             <MenuItem value="DVD">DVD</MenuItem>
@@ -107,6 +152,7 @@ export const AddMovieForm: FC<IAddMovieForm> = (props) => {
             name="firstName"
             value={firstActor.firstName}
             onChange={handleFirstActor}
+            required
           />
           <TextField
             label="last name"
@@ -115,32 +161,164 @@ export const AddMovieForm: FC<IAddMovieForm> = (props) => {
             name="lastName"
             value={firstActor.lastName}
             onChange={handleFirstActor}
+            required
           />
         </div>
-        <div className={classes.inputDivActor}>
-          <TextField
-            label="first name"
-            variant="outlined"
-            className={classes.inputFieldActor}
-            name="firstName"
-            value={secondActor.firstName}
-            onChange={handleSecondActor}
-          />
-          <TextField
-            label="last name"
-            variant="outlined"
-            className={classes.inputFieldActor}
-            name="lastName"
-            value={secondActor.lastName}
-            onChange={handleSecondActor}
-          />
+        {
+        !moreThanZero
+        && (
+        <div className={classes.plusButton}>
+          <Tooltip title="Add actor" placement="right">
+            <span>
+              <IconButton onClick={addCount} disabled={!canSubmit}>
+                <AddCircleOutlineIcon />
+              </IconButton>
+            </span>
+          </Tooltip>
         </div>
+        )
+        }
+        {
+          moreThanZero
+          && (
+          <div className={classes.inputDivActor}>
+            <TextField
+              label="first name"
+              variant="outlined"
+              className={classes.inputFieldActor}
+              name="firstName"
+              value={secondActor.firstName}
+              onChange={handleSecondActor}
+            />
+            <TextField
+              label="last name"
+              variant="outlined"
+              className={classes.inputFieldActor}
+              name="lastName"
+              value={secondActor.lastName}
+              onChange={handleSecondActor}
+            />
+          </div>
+          )
+        }
+        {
+        counterEqualOne
+        && (
+        <div>
+          <Tooltip title="Add actor" placement="right">
+            <span>
+              <IconButton onClick={addCount} disabled={!secondActorFilled}>
+                <AddCircleOutlineIcon />
+              </IconButton>
+            </span>
+          </Tooltip>
+        </div>
+        )
+        }
+        {
+          moreThanOne
+          && (
+          <div className={classes.inputDivActor}>
+            <TextField
+              label="first name"
+              variant="outlined"
+              className={classes.inputFieldActor}
+              name="firstName"
+              value={thirdActor.firstName}
+              onChange={handleThirdActor}
+            />
+            <TextField
+              label="last name"
+              variant="outlined"
+              className={classes.inputFieldActor}
+              name="lastName"
+              value={thirdActor.lastName}
+              onChange={handleThirdActor}
+            />
+          </div>
+          )
+        }
+        {
+        counterEqualTwo
+        && (
+        <div>
+          <Tooltip title="Add actor" placement="right">
+            <span>
+              <IconButton onClick={addCount} disabled={!thirdActorFilled}>
+                <AddCircleOutlineIcon />
+              </IconButton>
+            </span>
+          </Tooltip>
+        </div>
+        )
+        }
+        {
+          moreThanTwo
+          && (
+          <div className={classes.inputDivActor}>
+            <TextField
+              label="first name"
+              variant="outlined"
+              className={classes.inputFieldActor}
+              name="firstName"
+              value={fourthActor.firstName}
+              onChange={handleFourthActor}
+            />
+            <TextField
+              label="last name"
+              variant="outlined"
+              className={classes.inputFieldActor}
+              name="lastName"
+              value={fourthActor.lastName}
+              onChange={handleFourthActor}
+            />
+          </div>
+          )
+        }
+        {
+        counterEqualThree
+        && (
+        <div>
+          <Tooltip title="Add actor" placement="right">
+            <span>
+              <IconButton onClick={addCount} disabled={!fourthActorFilled}>
+                <AddCircleOutlineIcon />
+              </IconButton>
+            </span>
+          </Tooltip>
+        </div>
+        )
+        }
+        {
+          moreThanThree
+          && (
+          <div className={classes.inputDivActor}>
+            <TextField
+              label="first name"
+              variant="outlined"
+              className={classes.inputFieldActor}
+              name="firstName"
+              value={fifthActor.firstName}
+              onChange={handleFifthActor}
+            />
+            <TextField
+              label="last name"
+              variant="outlined"
+              className={classes.inputFieldActor}
+              name="lastName"
+              value={fifthActor.lastName}
+              onChange={handleFifthActor}
+            />
+          </div>
+          )
+        }
         <Button
           variant="contained"
           color="primary"
           type="submit"
           className={classes.btnSubmit}
           onClick={handleSubmit}
+          disabled={!canSubmit}
         >
           Submit
         </Button>
