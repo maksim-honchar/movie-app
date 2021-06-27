94ebb768ff64ab26d8c357b0b08f515556b1f690
Maksym Honchar <maksim.honchar@gmail.com>
2021-06-27 20:06:06 +0300

    added content to EmptyStorage page

--- src/view/EmptyStorage.tsx
+++ src/view/EmptyStorage.tsx
@@ -7,7 +7,10 @@ const useStyles = makeStyles({
   root: {
     display: 'flex',
     flexDirection: 'column',
-    marginTop: '30vh',
+    marginTop: '20vh',
+  },
+  titleIntro: {
+    marginTop: 70,
   },
 });
 
@@ -27,6 +30,31 @@ export const EmptyStorage = () => {
         fill the form or upload file
       </Typography>
       <Divider variant="middle" />
+      <Typography
+        variant="h6"
+        color="textSecondary"
+        align="center"
+        gutterBottom
+        className={classes.titleIntro}
+      >
+        Application architecture
+      </Typography>
+      <Typography color="textSecondary">
+        The application uses the following technology stack:
+        TypeScript, React, Redux, Material-UI, Firebase, react-router-dom, webpack.
+        <br />
+        The src folder contains folders and files responsible for UI and application logic.
+        <br />
+        The controllers folder contains components responsible for the application logic.
+        <br />
+        The view folder contains components that are responsible for displaying UI.
+        <br />
+        The redux folder contains a store and a reducer
+        <br />
+        The utils folder, as the name implies, contains constants, interfaces, a custom hook.
+        <br />
+        Firebase folder is responsible for firebase settings
+      </Typography>
     </div>
   );
 };
